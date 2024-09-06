43ae5fab20222d611d14d0f2176ffb31fdb79cd8
mmdreza zeydani <mmdzyzfrontendz2002@gmail.com>
2024-09-06 15:10:10 +0330

    i have changed the style of sign up component and made it like sign in page

--- src/components/signIn&signUp&signOut/signOut.tsx
+++ src/components/signIn&signUp&signOut/signOut.tsx
@@ -3,7 +3,7 @@ import { TData } from "../../types/types";
 import Loading from "../loading/Loading";
 import useGetSignedInUsers from "../../hooks/useGetSignedInUsers";
 import useSignOutUser from "../../hooks/useSignOutUser";
-import { useNavigate } from "react-router-dom";
+import { useNavigate,Link } from "react-router-dom";
 const SignOUt = () => {
     const navigate=useNavigate()
     const {data,isError,error,isLoading}=useGetSignedInUsers()
@@ -21,44 +21,50 @@ const SignOUt = () => {
         }
     }
     return ( 
-        <form onSubmit={handleSubmit(submit)} className="flex flex-col  items-center mt-2 mb-20">
-            {isError && <h1 className="error">{error?.message}</h1>}
-            {isLoading && <Loading/>}
-            <h2 className="text-xl xs:text-3xl">Sign Out Your Account</h2>
-        <div className="flex flex-col gap-y-4 justify-center items-start w-full px-2 xs:w-1/2 child:w-full child:input child:py-3 child:px-2">
-            <input placeholder="Email" type="email" {...register("email",{
-                  required:true,
-                  pattern:{
-                   value:/[\w]*@*[a-z]*\.*[\w]{5,}(\.)*(com)*(@gmail\.com)/g,
-                   message:"invalid format"
-                   },
-                  validate:{
-                   notAdmin:(field)=>{
-                       return (field !=="mmdzyzw033zeydani@gmail.com" || "try another email address")
-                   },
-                   notBlockListed:(field)=>{
-                       return (!field.endsWith("baddomain.com") || "unacceptable")
-                   }
-                }
-            })}/>
-            {
-                errors.email?.message ?errors.email.message:null
-             }
-         {errors.email && errors.email.type === "required" && "Email is required"}
-            <input placeholder="Password" type="password" {...register("password",{
-                required:true,
-                pattern:{
-                  value:/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/,
-                  message:"Password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter, one special character, no space, and it must be 8-16 characters long."
-                }
-            })} />
-               {errors.password && errors.password.type === "required" && "Password is required"}
-         {
+        <div className="flex flex-col  items-center mt-2 mb-20 ">
+          {isError && <h1 className="error">{error?.message}</h1>}
+          {isLoading && <Loading/>}
+        <form noValidate onSubmit={handleSubmit(submit)} className=" flex flex-col gap-y-6 justify-center items-start w-11/12 sm:w-1/2 lg:w-1/3 px-8 sm:px-12 py-5  text-white">
+        <h1 className="text-3xl">Sign Out</h1>
+       <div className="flex flex-col w-full gap-y-6  child:input child:py-3 xs:child:py-4 child:px-1 ">
+        <input  placeholder="Email"  type="email" {...register("email",{
+          required:true,
+          pattern:{
+           value:/[\w]*@*[a-z]*\.*[\w]{5,}(\.)*(com)*(@gmail\.com)/g,
+           message:"invalid format"
+           },
+          validate:{
+           notAdmin:(field)=>{
+               return (field !=="mmdzyzw033zeydani@gmail.com" || "try another email address")
+           },
+           notBlockListed:(field)=>{
+               return (!field.endsWith("baddomain.com") || "unacceptable")
+           }
+       }
+        })} />
+           {
+               errors.email?.message ?errors.email.message:null
+            }
+        {errors.email && errors.email.type === "required" && "Email is required"}
+        <input placeholder="Password" type="password" {...register("password",{
+         required:true,
+         pattern:{
+            value:/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/,
+            message:"Password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter, one special character, no space, and it must be 8-16 characters long."
+          }
+        })} />
+        {errors.password && errors.password.type === "required" && "Password is required"}
+        {
                 errors.password?.message ?errors.password.message:null
-             }
-        </div>
-        <input className="bg-red-700 active-btn rounded-md px-5 py-2 text-xl active-btn " value={"Sign Out"} type="submit" />
+        }
+       </div>
+       <input className='w-full bg-red-700 active-btn text-center py-3 xs:py-4 text-xl md:text-2xl rounded-md cursor-pointer' value={"Sign In"} type="submit" />
+       <div className="flex flex-col">
+       <div>Dont you want to sign out?</div>
+        <Link className="text-neutral-400 underline" to={"/sign_up"}>Back to home page</Link>
+       </div>
         </form>
+      </div>
      );
 }
  
