16584742c1083fe177d25ecdb0e90c8b701fcc29
mmdreza zeydani <mmdzyzfrontendz2002@gmail.com>
2024-09-08 02:18:55 +0330

    i protected dashboard route and sign out route, also create not found page route and component

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,22 @@ import Home from "./components/Home/home"
 import SignIn from "./components/signIn&signUp&signOut/signIn"
 import SignUp from "./components/signIn&signUp&signOut/signUp"
 import SignOUt from "./components/signIn&signUp&signOut/signOut"
-import { Route,Routes } from "react-router-dom"
+import { Route,Routes,Navigate } from "react-router-dom"
 import Footer from "./components/Home/footer/footer"
 import NetFlixDashBoard from "./components/netflix/netflix"
 import MyContextProvider from "./components/Home/context/myContext"
+import NotFoundPage from "./components/notFound"
 function App() {
+  const isAuth=localStorage.getItem("token")
   return (
     <MyContextProvider>
        <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/sign_up" element={<SignUp/>}/>
         <Route path="/sign_in" element={<SignIn/>}/>
-        <Route path="/netflix" element={<NetFlixDashBoard/>}/>
-        <Route path="/sign_out" element={<SignOUt/>}/>
+        <Route path="/netflix" element={isAuth ?<NetFlixDashBoard/>:<Navigate to={"/"}/>}/>
+        <Route path="/sign_out"element={isAuth ?<SignOUt/>:<Navigate to={"/"}/>}/>
+        <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
       <Footer/>
     </MyContextProvider>
---
+++ src/components/notFound.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.png"
+const NotFoundPage = () => {
+    return ( 
+        <div className="flex flex-col pb-5 items-center justify-center text-white ">
+            <img className="w-1/2" src={logo} alt="" />
+            <h1 className="text-2xl lg:text-5xl">404 This Page Does Not Exist</h1>
+            <Link className="bg-red-700 inline-block active-btn px-4 lg:px-8 py-2 my-8 lg:my-12 rounded-md " to={"/"}>Back To Home Page</Link>
+        </div>
+     );
+}
+ 
+export default NotFoundPage;
