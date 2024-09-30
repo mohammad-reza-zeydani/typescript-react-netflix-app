88ccec1113a1ebc9534179eb1a246860d0034c85
mmdreza zeydani <mmdzyzfrontendz2002@gmail.com>
2024-09-30 18:11:13 +0330

    changed the port in index.js file

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
 })
